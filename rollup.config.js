47d6047ea88775e1159831e9df2fed068a3b4f4e
Martin Jakobsen <martinjakobsen93@gmail.com>
2020-04-16 18:29:15 +0200

    Added rollup terser plugin to minify code. Changed entrypoint to minified file.

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,21 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "./src/components/index.js",
-  output: {
-    file: "./dist/index.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: "./dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "./dist/index.min.js",
+      format: "cjs",
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     postcss({
       modules: true,
